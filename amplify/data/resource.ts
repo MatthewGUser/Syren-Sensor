b3ea7c38356dd461ef50823d964a045b84bb54ed
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-11 20:32:14 -0400

    npm update

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -13,8 +13,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
 	schema,
 	authorizationModes: {
-		defaultAuthorizationMode: "apiKey",
-		// API Key is used for a.allow.public() rules
+		defaultAuthorizationMode: "userPool",
 		apiKeyAuthorizationMode: {
 			expiresInDays: 30,
 		},
