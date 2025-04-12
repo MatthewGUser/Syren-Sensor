54ad670b7165e0262a61148847fe10e41ae66202
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-11 23:56:57 -0400

    sandbox adjustments

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,8 +3,8 @@ import { referenceAuth } from "@aws-amplify/backend";
 export const auth = referenceAuth({
 	userPoolId: "us-east-1_XlaObV4IZ",
 	identityPoolId: "us-east-1:bdc6206d-a44b-420c-bb3d-d24b2f24dd72",
-	userPoolClientId: "4f2vad1m9feimkd77f3auj3fdd",
 	authRoleArn: "arn:aws:iam::767398061569:role/service-role/syren-identity-pool",
 	unauthRoleArn:
 		"arn:aws:iam::767398061569:role/service-role/syren-identity-guest",
+	userPoolClientId: "4f2vad1m9feimkd77f3auj3fdd",
 });
